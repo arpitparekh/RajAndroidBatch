94430bcf8b4f2f7456f2a1a96c534dc4d4b3fc46
arpitparekh <arpitparekh9@gmail.com>
2023-01-17 09:28:02 +0530

    datatype

---
+++ src/main/kotlin/datatype.kt
@@ -0,0 +1,111 @@
+fun main(){
+
+    // val
+    // var
+    // print()
+    // println()
+    // fun
+    // readLine()
+
+    val a = 12
+    val b = 13
+
+    // datatypes
+
+    // Numbers
+    // Boolean
+    // String
+    // Character
+    // Any
+
+    // Array
+    // ArrayList<>
+    // List<>
+    //HashMap<>
+    // HashSet<>
+
+    // Number
+    // Int  // Float // Double // Long  // Byte // Short
+
+    // Int
+
+    val c : Int = 12
+
+    val d : Int = 14
+
+    var g : Int = 56
+
+    val height = 34.56F   // 8 byte  // float
+
+    var width = 56.78  // 16 byte  // double
+
+    val currentTimeInMilli = 874356843L  // int
+
+    // Byte  (1 byte)  // Short (2 byte)
+
+    val y : Byte= 127
+
+    val s : Short = 23232
+
+    val number : Int  // declaration and initialization
+    number = 34
+
+    val range : Float
+    range = 56.67F
+
+    // re-initialization
+
+    var area : Double  // declaration
+
+    area = 45.67 // initialization
+
+    area = 99.89  // re-intilization
+
+    area = 45.0
+
+    var myData : Number = 45
+    myData = 45.67
+    myData = 4355435353453454545
+    myData = 12.34f
+    myData = 6756L
+
+    // Boolean
+
+    // true // false
+
+    var isLogin : Boolean = true
+    val isFirstTime = false
+
+    isLogin = false
+
+    // String
+
+    val firstName : String = "Raj"
+
+    val lastName : String  // declare
+
+    lastName = "Patel"  // initialization
+
+    // Character
+
+    val ch : Char = 'a'
+
+    var myChar : Char
+
+    // Any
+
+    var corona : Any
+    corona = 34
+    corona = 56.78
+    corona = 4354355343
+    corona = "Raj"
+    corona = 'f'
+    corona = false
+    corona = true
+
+    println(corona)
+
+//    val r : Float
+//    println(3.14*r*r)
+
+}
