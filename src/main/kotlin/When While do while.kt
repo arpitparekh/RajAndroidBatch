a66a4eb32c41d443a1a3436b5fb492cd6a291cb8
arpitparekh <arpitparekh9@gmail.com>
2023-02-05 16:05:18 +0530

    while loop programs

---
+++ src/main/kotlin/When While do while.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    // data types
+    // operators
+    // conditional statements  // if else // else if // else // when
+    // loops // for // when // do while
+
+    // math functions  // pow  // sqrt  // cbrt //
+
+    // string functions
+
+    // find the reverse of the number  // 4567
+
+    val number = readLine()!!.toInt()
+    
+
+}
---
+++ src/main/kotlin/find reverse of a number.kt
@@ -0,0 +1,40 @@
+fun main() {
+
+    // reverse
+
+    // palindrome number  // 12321  // 121  // 212  // 123454321
+
+    var a = readLine()!!.toInt()  // 1234
+    var reverse = 0
+    val originalValue = a
+
+    if(a!=null){
+
+        // 1234
+        // 1234 / 10 = 123
+        // 123 / 10 = 12
+        // 12 / 10 = 1
+        // 1 / 10 = 0
+
+        while(a != 0){   // 1234  // 123 // 12 // 1
+
+           val lastDigit = a%10  // 4  / 3  / 2  / 1
+
+            reverse = reverse*10 + lastDigit  // 4  //  43 // 432  // 4321
+
+            a = a/10  // 123 // 12 / 1 / 0
+
+        }
+        println(reverse)
+
+
+        if(reverse==originalValue){
+            println("number is palindrome")
+        }else{
+            println("number is not palindrome")
+        }
+
+    }
+
+
+}
